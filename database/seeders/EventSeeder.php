b805ec15ad097e553a199401f19c6ae167752f4b
shrall <shrallvierdo@gmail.com>
2021-05-11 01:43:20 +0700

    event - seeder

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use Illuminate\Database\Seeder;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $event = new Event();
+        $event->title = 'NPLC';
+        $event->custom_link = 'nplc2020';
+        $event->description = 'National Programming & Logic Competition';
+        $event->logo = "https://raw.githubusercontent.com/shrall/enpielsiweb/main/emaillogo.png";
+        $event->banner = "https://raw.githubusercontent.com/shrall/enpielsiweb/main/posternplc.png";
+        $event->template = 1;
+        $event->email = "mailto:shrallvierdo@gmail.com";
+        $event->instagram = "https://instagram.com/shrallok";
+        $event->whatsapp = "https://api.whatsapp.com/send?phone=6281233129966";
+        $event->date = "2021-05-15";
+        $event->title_color = "#000000FF";
+        $event->desc_color = "#000000FF";
+        $event->date_color = "#000000FF";
+        $event->contacts_color = "#000000FF";
+        $event->register = "1";
+        $event->register_text = "Register";
+        $event->register_button_color = "#000000FF";
+        $event->register_text_color = "#000000FF";
+        $event->user_id = 1;
+        $event->save();
+    }
+}
