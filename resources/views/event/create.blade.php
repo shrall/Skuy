f78392aafdc310df8b0208894bc5f20a51866a8b
shrall <shrallvierdo@gmail.com>
2021-05-14 04:12:46 +0700

    template fix

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -332,7 +332,8 @@
                             <div class="text-5xl text-dark-300 font-skuy-primary">Banner</div>
                             <span class="fa fa-image text-dark-300 text-5xl"></span>
                         </div>
-                        <div class="row-span-3 flex flex-col justify-start font-sans text-left px-12 mt-8 w-full overflow-hidden">
+                        <div
+                            class="row-span-3 flex flex-col justify-start font-sans text-left px-12 mt-8 w-full overflow-hidden">
                             <div v-show='eventTitle == ""' v-bind:style="{color: titleColor}"
                                 class="text-4xl font-semibold text-left mb-2 break-words">Judul
                             </div>
@@ -364,8 +365,7 @@
                                 class="text-white text-center py-2 px-2 rounded-lg w-40 font-medium text-xl bg-secondary-100 cursor-pointer break-words">
                                 @{{ registerText }}</div>
                         </div>
-                        <div v-show="logoBool"
-                            class="rounded-lg bg-white absolute top-12 left-16 w-24 p-1 shadow-2xl">
+                        <div v-show="logoBool" class="rounded-lg bg-white absolute top-12 left-16 w-24 p-1 shadow-2xl">
                             <img class="templateLogo rounded-lg">
                         </div>
                         <div v-show="!logoBool"
@@ -488,7 +488,8 @@
                         {{-- <div class="text-5xl text-dark-300 font-skuy-primary">Banner</div>
                         <span class="fa fa-image text-dark-300 text-5xl"></span> --}}
                     </div>
-                    <div class="row-span-3 flex flex-col justify-start font-sans text-left px-12 mt-8 w-full overflow-hidden">
+                    <div
+                        class="row-span-3 flex flex-col justify-start font-sans text-left px-12 mt-8 w-full overflow-hidden">
                         <div v-show='eventTitle == ""' v-bind:style="{color: titleColor}"
                             class="text-4xl font-semibold text-left mb-2 break-words">Judul
                         </div>
@@ -583,11 +584,60 @@
                 <!-- note template 2 -->
                 <div v-show="eventTemplate == 2"
                     class="text-center rounded-lg bg-gray-50 w-1/4 h-full shadow-2xl mx-auto flex relative">
-                    <div class="flex flex-col justify-center font-sans text-left px-8 z-40">
+                    <div class="flex flex-col justify-center font-sans text-left z-40 overflow-hidden">
                         <div v-show='eventTitle == ""' v-bind:style="{color: titleColor}"
-                            class="text-3xl font-semibold text-left mb-2 break-words">Judul
+                            class="text-3xl font-semibold text-left mb-2 break-words mx-8 z-40">Judul
                         </div>
-                        <div class="text-3xl font-semibold text-left mb-2 break-words"
+                        <div class="text-3xl font-semibold text-left mb-2 break-words mx-8 z-40"
+                            v-bind:style="{color: titleColor}">@{{ eventTitle }}</div>
+                        <span v-show='eventDesc == ""' v-bind:style="{color: descColor}"
+                            class="text-sm text-left mb-2 break-words mx-8 z-40">Deskripsikan eventmu
+                            disini.</span>
+                        <span class="text-sm text-left mb-2 break-words mx-8 z-40"
+                            v-bind:style="{color: descColor}">@{{ eventDesc }}</span>
+                        <span class="text-lg text-left mb-2 mx-8 z-40" v-bind:style="{color: dateColor}">
+                            <span class="fa fa-calendar-day text-xl"></span> @{{ eventDate }}</span>
+                        <div class="flex items-left space-x-8 text-4xl my-4 mx-8 z-40" v-bind:style="{color: contactsColor}">
+                            <a v-if="emailBool" target="_blank" v-bind:href="eventEmail">
+                                <span
+                                    class="fa fa-envelope hover:text-gray-600 transition ease-in-out duration-500"></span>
+                            </a>
+                            <a v-if="instagramBool" target="_blank" v-bind:href="eventInstagram">
+                                <span
+                                    class="fab fa-instagram hover:text-gray-600 transition ease-in-out duration-500"></span>
+                            </a>
+                            <a v-if="whatsappBool" target="_blank" v-bind:href="eventWhatsapp">
+                                <span
+                                    class="fab fa-whatsapp hover:text-gray-600 transition ease-in-out duration-500"></span>
+                            </a>
+                        </div>
+                        <div v-show="registerBool"
+                            v-bind:style="{'background-color': registerButtonColor, color:registerTextColor}"
+                            class="text-white text-center py-2 px-2 rounded-lg w-40 font-medium text-xl bg-secondary-100 cursor-pointer break-words mx-8 z-40">
+                            @{{ registerText }}</div>
+                        <div v-show="bannerBool" class="bg-black h-full w-full absolute opacity-20 z-10">
+                        </div>
+                    </div>
+                    <div v-show="bannerBool" class="absolute flex flex-col justify-center overflow-hidden h-full">
+                        <img class="templateBanner my-auto">
+                    </div>
+                    <div v-show="logoBool"
+                        class="rounded-lg bg-white absolute top-12 left-52 right-0 w-24 p-1 shadow-2xl mx-auto z-50">
+                        <img class="templateLogo rounded-lg">
+                    </div>
+                </div>
+                <!-- note template 3 -->
+                <div v-show="eventTemplate == 3"
+                    class="text-center rounded-lg bg-gray-50 w-1/4 h-full shadow-2xl py-8 mx-auto grid grid-rows-6">
+                    <div v-show="bannerBool"
+                        class="row-span-2 flex flex-col justify-center px-2 overflow-hidden h-full">
+                        <img class="templateBanner object-contain my-auto">
+                    </div>
+                    <div class="row-span-4 flex flex-col justify-start font-sans text-left w-full px-8 overflow-hidden">
+                        <div v-show='eventTitle == ""' v-bind:style="{color: titleColor}"
+                            class="text-2xl font-semibold text-left mb-2 break-words">Judul
+                        </div>
+                        <div class="text-2xl font-semibold text-left mb-2 break-words"
                             v-bind:style="{color: titleColor}">@{{ eventTitle }}</div>
                         <span v-show='eventDesc == ""' v-bind:style="{color: descColor}"
                             class="text-sm text-left mb-2 break-words">Deskripsikan eventmu
@@ -596,7 +646,7 @@
                             v-bind:style="{color: descColor}">@{{ eventDesc }}</span>
                         <span class="text-lg text-left mb-2" v-bind:style="{color: dateColor}">
                             <span class="fa fa-calendar-day text-xl"></span> @{{ eventDate }}</span>
-                        <div class="flex items-left space-x-8 text-4xl my-4" v-bind:style="{color: contactsColor}">
+                        <div class="flex items-left space-x-8 text-4xl mb-2" v-bind:style="{color: contactsColor}">
                             <a v-if="emailBool" target="_blank" v-bind:href="eventEmail">
                                 <span
                                     class="fa fa-envelope hover:text-gray-600 transition ease-in-out duration-500"></span>
@@ -615,13 +665,8 @@
                             class="text-white text-center py-2 px-2 rounded-lg w-40 font-medium text-xl bg-secondary-100 cursor-pointer break-words">
                             @{{ registerText }}</div>
                     </div>
-                    <div v-show="bannerBool" class="absolute flex flex-col justify-center overflow-hidden h-full">
-                        <img class="templateBanner my-auto">
-                        <div v-show="bannerBool" class="bg-black rounded-xl h-full w-full absolute opacity-20">
-                        </div>
-                    </div>
                     <div v-show="logoBool"
-                        class="rounded-lg bg-white absolute top-12 left-52 right-0 w-24 p-1 shadow-2xl mx-auto">
+                        class="rounded-lg bg-white absolute top-12 left-0 right-52 w-24 p-1 shadow-2xl mx-auto">
                         <img class="templateLogo rounded-lg">
                     </div>
                 </div>
